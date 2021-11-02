e575a59357bdb65b2cd02c94ce36dd27e8263ed6
Sohail Shaukat Selod <sohail47k@gmail.com>
2021-11-03 01:52:06 +0530

    sync

---
+++ mine/pages/index.js
@@ -0,0 +1,31 @@
+import { getFeaturedEvents } from "../helpers/api-util";
+import EventList from "../components/events/event-list";
+import Head from "next/head";
+
+function HomePage(props) {
+  return (
+    <div>
+      <Head>
+        <title>NextJS Events</title>
+        <meta
+          name="description"
+          content="Find alot of great events that allow you to evolve."
+        />
+      </Head>
+      <EventList items={props.events} />
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+      events: featuredEvents,
+    },
+    revalidate: 1800,
+  };
+}
+
+export default HomePage;
